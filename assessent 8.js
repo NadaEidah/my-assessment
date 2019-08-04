e7594ce41bc624f2b06736f57be046b375710328
NadaEidah <nada.eadah@gmail.com>
2019-08-04 11:34:57 +0300

    nada

---
+++ assessent 8.js
@@ -0,0 +1,37 @@
+//q1
+
+Animal( animalName, type){
+ var animalName =animalName
+ 
+
+$("Animal").append("createElement[i]");
+ 
+var animals = {
+{Name:"anything", type:"type1"}
+}
+
+$("animals()").append("Animal()");
+}
+
+//q2 
+
+const shape=() =>{
+    this.height = "13CM"
+    this.length = "12CM"
+    this.area = "256CM2"
+    getteArea(area){
+       return area; 
+    }
+    
+}
+shape1=new shape1();
+$("shape()").append("shape1");
+
+
+//q3
+
+const arrow =(arr) =>{
+    newArrow=arr.filter(i=>i.length<=4)
+    return arr; 
+}
+
